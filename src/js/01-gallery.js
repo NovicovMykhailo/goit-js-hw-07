4768be4e64399d0a138caaca2f5ae17c005adfcd
NovicovMykhailo <mikenovicov@gmail.com>
2023-02-22 22:51:44 +0200

    1st part done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,39 +4,57 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 /*
 <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href=${el.original}>
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src=${el.preview}
+      data-source=${el.original}
+      alt=${el.description}
     />
   </a>
 </div>*/
 
-
-
+//  --------- Gallery Container Selection ------ 
 const container = document.querySelector(".gallery");
+
+// ======  event listener
 container.addEventListener("click", onClick);
 
-
+// ======  gallery layout function
 function createGallery(object) {
-	let galleryList = [];
-	for (let el of object) {
-		let listEl;
-		listEl = `<div class="gallery__item">
-            <a class="gallery__link" href=${el.original}>
-            <img class="gallery__image" src=${el.preview} 
-            data-source="${el.original}" alt=${el.description}/>
-            </a>
-        </div>`;
-		galleryList.push(listEl);
-	}
-	return galleryList;
+	return object.map(
+		el =>
+			`<div class="gallery__item">
+      <a class="gallery__link" href=${el.original}>
+        <img
+            class="gallery__image"
+            src=${el.preview}
+            data-source=${el.original}
+            alt=${el.description}
+        />
+      </a>
+    </div>`,
+	);
 }
-const createMackup = createGallery(galleryItems)
+
+// ======   inserting a layout into the DOM
+const createMackup = createGallery(galleryItems);
 container.insertAdjacentHTML("afterbegin", createMackup.slice(",").join(" "));
 
+// ======   callback Functions
 function onClick(event) {
 	event.preventDefault();
+	if (event.target.className !== "gallery__image") {
+		return;
+	}
+	basicLightbox.create(`<img src="${event.target.dataset.source}" width="1280">`).show(onClose);
 }
+
+function onClose(click) {
+	window.addEventListener("keydown", event => {
+		if (event.key === "Escape") {
+			return click.close();
+		}
+	});
+}
+
